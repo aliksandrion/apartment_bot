fcfab42397de0331c4e04db9826f4d9b087aa481
aliksandrion <aliksandrion@gmail.com>
2022-04-14 13:53:58 +0300

    UPD

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import requests
+API_link = 'https://api.telegram.org/bot5131345127:AAHyS8vPIt7e24EPBbajSt2EsPXtdeFjYsc'
+updates = requests.get(API_link + '/getUpdates').json()
 
+print(updates)
+
+
+
+# from settings import bot_token
+
+# for i in range(5):
+#     requests.get(f'https://api.telegram.org/bot{bot_token}/sendMesage', params={'chat_id': 849467226, 'text': 'hey'})
