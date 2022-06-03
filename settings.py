c7566db10f936e8c8c15d767ca7e65d914e1abbe
aliksandrion <aliksandrion@gmail.com>
2022-06-03 13:20:07 +0300

    Add settings.py to .gitognore

---
+++ settings.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = '5131345127:AAHyS8vPIt7e24EPBbajSt2EsPXtdeFjYsc'
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko)'
+                         'Chrome/80.0.3987.149 Safari/537.36'}
+
+CITY_CODE = {
+    'tbilisi': 'Tbilisi?Keyword=Tbilisi&AdTypeID=3&PrTypeID=1&cities=1996871&GID=1996871&WithPhoto=1',
+    'batumi': 'Batumi?Keyword=Batumi&AdTypeID=3&PrTypeID=1&cities=8742159&GID=8742159&WithPhoto=1',
+    'yerevan': '&n=1',
+    'gyumri': '&n=51',
+}
