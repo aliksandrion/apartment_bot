63075f5077652ade8a4ee637ce7e363706cce025
aliksandrion <aliksandrion@gmail.com>
2022-06-04 14:47:07 +0300

    1) Some corrects in the main.py 2) Corrected descriptions and added gif to README.md

--- README.md
+++ README.md
@@ -1,36 +1,37 @@
-# Project Title
+# Apartment Bot
 A Telegram Python bot for searching apartments in Georgia and Armenia.
-
+![](Pres.gif)
 ### Getting Started
-1) Send /newbot command to @BotFather and name your bot, e.g. ApartmentBot
-2) Send username for your bot. It must ends with 'bot', e.g. apartment_bot   
-3) Copy HTTP API token and put it in 'setting.py'. Save token as 'BOT_TOKEN'
+1) Send a '/newbot' command to @BotFather and name your bot, e.g. ApartmentBot
+2) Choose a username for your bot. It must end with 'bot', e.g. apartment_bot   
+3) Copy HTTP API token and insert it in 'setting.py'. Save the token as 'BOT_TOKEN'
 
 ### Prerequisites
-All modules for successful work are in 'requirements.txt'.  
-Run following command in CMD/Console to install:
+All the modules for successful work are in 'requirements.txt'.  
+Run the following command in CMD/Console to install:
 ```
 pip install -r requirements.txt
 ```
 
 ### Installing
-Open terminal and go to the directory you need.  
-Enter command:  
+Open a terminal and go to the directory you need.  
+Enter the command:  
 ```
 git clone git@github.com:aliksandrion/apartment_bot.git
 ```
-Enter next command after:
+Enter the next command:
 ```
 python3 -m venv venv
 source venv/bin/activate
 cd MyBlog
 pip install -r requirements.txtt
 ```
-Create the file 'settings.py' and put 'BOT_TOKEN' from @BotFather, e.g.:
+Create a file 'settings.py' and insert 'BOT_TOKEN' from @BotFather.  
+Its structure should be the following:
 ```
 BOT_TOKEN = '0123456789:AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqR'
 ```
-In the 'settings.py' copy that parameters:
+In the 'settings.py' insert these parameters:
 ```
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko)'
                          'Chrome/80.0.3987.149 Safari/537.36'}
@@ -42,13 +43,13 @@ CITY_CODE = {
     'gyumri': '&n=51',
 }
 ```
-'HEADERS' ensures avoiding a server blocks.
-'CITY_CODE' uses for url-requests to websites.
+'HEADERS' ensures avoiding server blocks.
+'CITY_CODE' specifies url-requests to websites.
 
 ### Built With
-* [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) - interface created for developers keen on building bots for Telegram.
-* [beautifulsoup4](https://www.crummy.com/software/BeautifulSoup/) - scraping information from web pages
-* [requests](https://pypi.org/project/requests/) - allows to send HTTP/1.1 requests
+* [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) - interface created for developers keen on building Telegram bots.
+* [beautifulsoup4](https://www.crummy.com/software/BeautifulSoup/) - scraping information from web pages.
+* [requests](https://pypi.org/project/requests/) - enables the developer to send HTTP/1.1 requests.
 
 ### Links of author
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from settings import BOT_TOKEN, HEADERS, CITY_CODE
 
-from pyTelegramBotAPI import telebot
+import telebot
 from telebot import types
 from telebot.types import InputMediaPhoto
 
@@ -11,7 +11,7 @@ from time import sleep
 
 bot = telebot.TeleBot(BOT_TOKEN)
 url = 'https://www.'
-count_of_ad = 0  # counter for sending 5 messages
+count_of_ad = 0  # number of messages sent
 page_counter = 1
 
 
@@ -27,10 +27,10 @@ def start(message):
     arm = types.InlineKeyboardButton(text='Armenia 🇦🇲', callback_data='arm')
     buttons.add(geo, arm)
     bot.send_message(message.chat.id, "Hey! I'm your assistant for searching apartments in Georgia and Armenia🏠"
-                                      "\n\nFirst, choose a country 🛫", reply_markup=buttons)
+                                      "\n\nFirst, choose the country 🛫", reply_markup=buttons)
 
 
-# responds for all callbacks in the bot
+# responsible for all callbacks
 @bot.callback_query_handler(func=lambda callback: callback.data)
 def check_callback_data(callback):
     global url
@@ -38,10 +38,10 @@ def check_callback_data(callback):
         url += 'myhome.ge/en/s/Apartment-for-rent-'
         geo_buttons = types.InlineKeyboardMarkup(row_width=2)
         tbilisi = types.InlineKeyboardButton(text='Tbilisi 🍷', callback_data='tbilisi')
-        batumi = types.InlineKeyboardButton(text='Батуми 🏖️', callback_data='batumi')
+        batumi = types.InlineKeyboardButton(text='Batumi 🏖️', callback_data='batumi')
         geo_buttons.add(tbilisi, batumi)
         bot.edit_message_text(chat_id=callback.message.chat.id,
-                              text='Press /restart to start over\n\nWhich georgian city?',
+                              text='Press /restart to start over\n\nWhich Georgian city?',
                               message_id=callback.message.id, reply_markup=geo_buttons)
 
     elif callback.data == 'arm':
@@ -51,13 +51,13 @@ def check_callback_data(callback):
         gyumri = types.InlineKeyboardButton(text='Gyumri ⛰️', callback_data='gyumri')
         arm_buttons.add(yerevan, gyumri)
         bot.edit_message_text(chat_id=callback.message.chat.id,
-                              text='Press /restart to start over\n\nWhich armenian city?',
+                              text='Press /restart to start over\n\nWhich Armenian city?',
                               message_id=callback.message.id, reply_markup=arm_buttons)
 
     elif callback.data in CITY_CODE:
         url += CITY_CODE.get(callback.data)
         msg = bot.edit_message_text(chat_id=callback.message.chat.id,
-                                    text='Press /restart to start over\n\nHow many rooms do you need?',
+                                    text='Press /restart to start over\n\nHow many rooms do you consider?',
                                     message_id=callback.message.id)
         bot.register_next_step_handler(msg, number_of_rooms)
 
@@ -68,7 +68,7 @@ def check_callback_data(callback):
             arm_ad(callback)
 
 
-# if user made a typo when typing a value of cost or a number of rooms
+# if a user makes a typo when choosing the options
 @bot.message_handler(content_types=['text'])
 def typo(message):
     typo_msg = bot.send_message(message.chat.id, 'Choose option higher')
@@ -77,20 +77,20 @@ def typo(message):
     bot.delete_message(message.chat.id, message.message_id)
 
 
-# user enters a number of rooms
+# a user enters a number of rooms
 def number_of_rooms(message):
     global url
     numb_of_rooms = message.text
-    # that block works if user enters any other message except digits from 1 to 4
+    # that block works if the user enters any other symbols except digits from 1 to 4
     if numb_of_rooms not in ['1', '2', '3', '4']:
         room_amount = bot.send_message(message.chat.id, 'Enter the number of rooms from 1 to 4')
         sleep(2)
-        bot.delete_message(message.chat.id, room_amount.message_id)  # delete incorrect message from user
-        bot.delete_message(message.chat.id, message.message_id)  # delete notification about wrong input
+        bot.delete_message(message.chat.id, room_amount.message_id)  # deletes incorrect messages
+        bot.delete_message(message.chat.id, message.message_id)  # deletes notifications about wrong inputs
         bot.register_next_step_handler(room_amount, number_of_rooms)
     else:
         msg_rooms = bot.send_message(message.chat.id, 'Enter the max cost per month:')
-        if 'myhome.ge' in url:  # add to url params depend on country
+        if 'myhome.ge' in url:  # adds to url the parameter depending on the country
             url += f'&RoomNums={numb_of_rooms}'
         elif 'list.am' in url:
             url += f'&_a4={numb_of_rooms}'
@@ -100,15 +100,15 @@ def number_of_rooms(message):
 def cost(message):
     global url
     max_price = message.text
-    if not max_price.isdigit():  # that block works if user enters any other messages except digits
+    if not max_price.isdigit():  # that block works if a user enters any other messages except digits
         cost_amount = bot.send_message(message.chat.id, 'Enter a numeric value\ne.g.: 300')
         sleep(2)
-        bot.delete_message(message.chat.id, cost_amount.message_id)  # delete incorrect message from user
-        bot.delete_message(message.chat.id, message.message_id)  # delete notification about wrong input
+        bot.delete_message(message.chat.id, cost_amount.message_id)  # deletes incorrect messages
+        bot.delete_message(message.chat.id, message.message_id)  # deletes notifications about wrong inputs
         bot.register_next_step_handler(cost_amount, cost)
     else:
         output = bot.send_message(message.chat.id, "Looking for suitable options...🔎")
-        if 'myhome.ge' in url:  # add to url params depend on country
+        if 'myhome.ge' in url:  # adds to url the parameter depending on the country
             url += f'&FCurrencyID=1&FPriceTo={max_price}'
             geo_ad(output)
         elif 'list.am' in url:
@@ -116,7 +116,7 @@ def cost(message):
             arm_ad(output)
 
 
-# searches the georgian website with a list of ad
+# searches the Georgian website with a list of ads
 def geo_parser():
     request = requests.get(url, headers=HEADERS)
     soup = b(request.text, 'html.parser')
@@ -124,45 +124,45 @@ def geo_parser():
     return items
 
 
-# parse each georgian ad and finds the necessary parameters
+# parses the ads according to the parameters
 def geo_ad(message):
     global count_of_ad, url, page_counter
     list_of_geo_ad = geo_parser()
     for i in list_of_geo_ad[count_of_ad:count_of_ad + 5]:
-        # parse params from cards of ads
+        # parses the parameters from the card in the list of ads
         price = i.find('b', class_='item-price-usd').text
         room = i.find('div', {'data-tooltip': "Number of rooms"}).text.strip('Room ')
         square = i.find('div', class_='item-size').text
         floor = i.find('div', class_='options-texts').text.strip('Floor ')
-        # parse photo in each card of ads
+        # opens the ad and parses photos
         link = i.get('href')
         request = requests.get(link, headers=HEADERS)
         soup = b(request.text, 'html.parser')
         img = soup.find('div', class_='swiper-wrapper')
         img = img.findChildren('img')[0:5]
         img = [InputMediaPhoto(item['data-src']) for item in img]
-        # text for output in the bot`s messages
+        # text for output bot`s messages
         text = f"Price: {price}$ pcm\nRooms: {room}\nSquare: {square}\nFloor: {floor}\n<a href='{link}'>🔗 LINK 🔗</a>"
 
-    # send an info and photos of ads to user:
-        if count_of_ad == 0 and '&Page=' not in url:  # chat_id takes from message for first 5 ads
+    # sends the info and photos from ads to the user:
+        if count_of_ad == 0 and '&Page=' not in url:  # chat_id works for 5 first messages
             chat_id = message.chat.id
-        else:  # another 5 ads will take from callback
+        else:  # each other 5 ads come from the callback
             chat_id = message.from_user.id
 
         try:
             bot.send_media_group(chat_id, media=img)
-        except telebot.apihelper.ApiTelegramException:  # some photos of ads can`t parsed - for that's cases use this
+        except telebot.apihelper.ApiTelegramException:  # some photos from ads can`t be parsed - for that case use this:
             bot.send_message(chat_id, 'No photo 🚫\n\n' + text, parse_mode='html', disable_web_page_preview=True)
         else:
             bot.send_message(chat_id, text, parse_mode='html', disable_web_page_preview=True)
 
-        # count_of_ad helps show by 5 ads
+        # limits the number of messages sent per time
         try:
             if i == list_of_geo_ad[count_of_ad + 4]:
                 count_of_ad += 5
                 show_more_ad(message)
-        except IndexError:  # that`s condition works if groups of 5 ads were showed from site`s page
+        except IndexError:  # if there are fewer than 5 ads in the end
             if i == list_of_geo_ad[count_of_ad]:
                 continue
             else:
@@ -172,7 +172,7 @@ def geo_ad(message):
                 show_more_ad(message)
 
 
-# searches the armenian website with a list of ad
+# searches the Armenian website with a list of ads
 def arm_parser():
     request = requests.get(url, headers=HEADERS)
     soup = b(request.text, 'html.parser')
@@ -180,47 +180,47 @@ def arm_parser():
     return items
 
 
-# parse each armenian ad and finds the necessary parameters
+# parses the ads according to the parameters
 def arm_ad(message):
     global count_of_ad, url, page_counter
     list_of_arm_ad = arm_parser()
     for i in list_of_arm_ad[count_of_ad:count_of_ad + 5]:
-        # parse params from cards of ads
+        # parses the parameters from the card in the list of ads
         price = i.find('div', class_='p').text.strip(' ֏ monthly').replace(',', '')
         info_flat = i.find('div', class_='at').text.split(',')
         rooms = info_flat[1].strip(' rm.')
         square = info_flat[2]
         floor = info_flat[3].strip(' floor')
-        # parse photo in each card of ads
+        # opens the ad and parses photos
         link = 'https://www.list.am/' + i.get('href')
         request = requests.get(link, headers={'user-agent': 'Mozilla/5.0'})
         img = re.findall(r'img:\["//(.*)"]', request.text)
         img = [i.split('","//') for i in img]
         img = [InputMediaPhoto(item) for item in img[0][0:5]]
-        # text for output in the bot`s messages
+        # text for output bot`s messages
         text = f"Price: {round(int(int(price) * dollar_to_dram()), -1)}$ pcm\nRooms: {rooms}\nSquare:{square}\n" \
                f"Floor: {floor}\n<a href='{link}'>🔗 LINK 🔗</a>"
 
-    # send an info and photos of ads to user:
+    # sends the info and photos from ads to the user:
         if count_of_ad == 0:  # chat_id takes from message for first 5 ads
             chat_id = message.chat.id
-        else:  # another 5 ads will take from callback
+        else:  # each other 5 ads come from the callback
             chat_id = message.from_user.id
 
         try:
             bot.send_media_group(chat_id, media=img)
-        except telebot.apihelper.ApiTelegramException:  # some photos of ads can`t parsed - for that's cases use this
+        except telebot.apihelper.ApiTelegramException:  # some photos from ads can`t be parsed - for that case use this:
             bot.send_message(chat_id, 'No photo 🚫\n\n' + text, parse_mode='html',
                              disable_web_page_preview=True)
         else:
             bot.send_message(chat_id, text, parse_mode='html', disable_web_page_preview=True)
 
-        # count_of_ad helps show by 5 ads
+        # limits the number of messages sent per time
         try:
             if i == list_of_arm_ad[count_of_ad + 4]:
                 count_of_ad += 5
                 show_more_ad(message)
-        except IndexError:  # that`s condition works if groups of 5 ads were showed from site`s page
+        except IndexError:  # if there are fewer than 5 ads in the end
             if i == list_of_arm_ad[count_of_ad]:
                 continue
             else:
@@ -230,7 +230,7 @@ def arm_ad(message):
                 show_more_ad(message)
 
 
-# func for currency transfer from armenian dram to dollar (site can`t do that)
+# currency transfers from the Armenian dram to the U.S. dollar (the website can not do that)
 def dollar_to_dram():
     google_request = requests.get('https://www.google.com/search?q=1+dram+in+usd', headers=HEADERS)
     soup = b(google_request.text, 'html.parser')
